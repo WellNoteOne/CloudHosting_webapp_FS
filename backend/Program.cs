6965077c26ab41cbab1b03a3d9cf7cff0d921e08
WellNoteOne <165422516+WellNoteOne@users.noreply.github.com>
2025-08-15 13:31:31 +0300

    added path reading module for program

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,3 @@
-using System;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -10,7 +8,17 @@ app.Run();
 
 string Hell()
 {
+    //First way
+    var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
+    var filePath = Path.Combine(helloFolder.FullName, "file.txt");
 
-    var message = System.IO.File.ReadAllText("./file.txt");
+    //Short way
+    var helloPath = Path.Combine(Directory.GetCurrentDirectory(), "file.txt");
+
+    // print to console path of the file
+    Console.WriteLine($"Reading 1st hello from: {filePath}");
+    Console.WriteLine($"Reading 2nd hello from: {helloPath}");
+
+    var message = File.ReadAllText(helloPath);
     return message + "!";
 }
