2d1783ac5f1a114de66483002c57b1d3f34a9933
WellNoteOne <165422516+WellNoteOne@users.noreply.github.com>
2025-08-15 12:23:35 +0300

    adding new function

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/hell", () => "Hell");
+app.MapGet("/", () => "Hello World!"); // Using anonomous functions
+app.MapGet("/hell", Hell);
 
 app.Run();
+
+string Hell()
+{
+    var message = "Hell";
+    return message + "!";
+}
